b974e1b678a19373a0686cf10bc5ce5ffcd93ff9
Mihail Radkov <mihail.radkov@ontotext.com>
2020-09-01 09:26:24 +0300

    Updated server build

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
+
+const BUILD_FOLDER = path.resolve('./build');
 const STATIC_FOLDER = path.resolve('./static');
 
-// app.use(express.json());
+app.use(express.json());
+app.use(express.static(BUILD_FOLDER));
 app.use('/api/static', express.static(STATIC_FOLDER));
+
+// Handles any requests that don't match the ones above by serving them the index page
+// This is the fallback for single page apps
+app.get('*', (req, res) => {
+    res.sendFile(BUILD_FOLDER + '/index.html');
+});
 
 app.listen(PORT, () => {
     console.log('Running server on ' + PORT);
